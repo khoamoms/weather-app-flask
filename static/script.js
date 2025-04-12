3c5b4a3c1854d5082f10d6d445d6a4e1bc08860b
khoamoms <165376371+khoamoms@users.noreply.github.com>
2025-04-12 15:25:56 +0700

    Update script.js

--- static/script.js
+++ static/script.js
@@ -11,14 +11,20 @@ function getWeather() {
             alert(data.error);
             return;
         }
+
         const container = document.getElementById('weatherCardContainer');
         container.innerHTML = `
             <div class="weather-card">
                 <h2>${data.city}</h2>
                 <p><img src="https://openweathermap.org/img/wn/${data.icon}@2x.png"> ${data.description}</p>
-                <p>Nhiệt độ: ${data.temp}°C</p>
+                <p><strong>Nhiệt độ:</strong> ${data.temp}°C</p>
+                <p><strong>Cảm giác như:</strong> ${data.feels_like}°C</p>
+                <p><strong>Độ ẩm:</strong> ${data.humidity}%</p>
+                <p><strong>Gió:</strong> ${data.wind_speed} m/s</p>
+                <p><strong>Áp suất:</strong> ${data.pressure} hPa</p>
             </div>
         `;
+
         document.getElementById('successMessage').style.display = 'block';
         document.getElementById('successMessage').innerText = 'Dữ liệu đã được tải thành công!';
     })
